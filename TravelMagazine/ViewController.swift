6e5271d96fb987e9dbfb7e422f5e412eb460ff58
lcho3878 <leech3878@naver.com>
2024-05-25 13:35:52 +0900

    Initial Commit

---
+++ TravelMagazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelMagazine
+//
+//  Created by 이찬호 on 5/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
