5d1b048482ce6e9eded3f489c72a469a77de9405
lcho3878 <leech3878@naver.com>
2024-05-25 13:38:33 +0900

    Magazine 구조체 생성

---
+++ TravelMagazine/Magazine.swift
@@ -0,0 +1,16 @@
+//
+//  Magazine.swift
+//  TravelMagazine
+//
+//  Created by 이찬호 on 5/25/24.
+//
+
+import Foundation
+struct Magazine {
+    let title: String
+    let subtitle: String
+    let photo_image: String
+    let date: String
+    let link: String
+}
+
