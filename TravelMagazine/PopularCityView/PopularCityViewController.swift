0a03f24139ac4c589af6cf9897a9bc1144f7cf46
lcho3878 <leech3878@naver.com>
2024-05-27 19:20:20 +0900

    스와이프 삭제 및 좋아요 구현

--- TravelMagazine/PopularCityView/PopularCityViewController.swift
+++ TravelMagazine/PopularCityView/PopularCityViewController.swift
@@ -55,6 +55,24 @@ extension PopularCityViewController: UITableViewDelegate, UITableViewDataSource 
 
     }
     
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = UIContextualAction(style: .destructive, title: "삭제") { _,_,_ in
+            self.list.remove(at: indexPath.row)
+            self.cityTableView.reloadData()
+        }
+        let action = UISwipeActionsConfiguration(actions: [delete])
+        return action
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let like = UIContextualAction(style: .normal, title: "좋아요") { _,_,_ in
+            self.list[indexPath.row].like?.toggle()
+            self.cityTableView.reloadData()
+        }
+        let action = UISwipeActionsConfiguration(actions: [like])
+        return action
+    }
+    
     @objc func heartButtonClicked(_ sender: UIButton) {
         list[sender.tag].like?.toggle()
         cityTableView.reloadData()
