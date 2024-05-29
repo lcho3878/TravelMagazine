56909766f6f1b7bb72ed1cde680e0cd07b63d8fb
lcho3878 <leech3878@naver.com>
2024-05-29 17:54:32 +0900

    파일 정리 및 CityInfo 파일 추가 City 구조체 생성

---
+++ TravelMagazine/Model/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  TravelMagazine
+//
+//  Created by 이찬호 on 5/29/24.
+//
+
+import Foundation
+
+struct City {
+    let city_name: String
+    let city_english_name: String
+    let city_explain: String
+    let city_image: String
+    let domestic_travel: Bool
+}
---
+++ TravelMagazine/PopularCityView/PopularCityViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PopularCityViewController.swift
+//  TravelMagazine
+//
+//  Created by 이찬호 on 5/29/24.
+//
+
+import UIKit
+
+class PopularCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "인기 도시"
+    }
+
+
+}
