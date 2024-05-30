a1379a58feddf25cec7e444857857292209be006
lcho3878 <leech3878@naver.com>
2024-05-30 20:10:34 +0900

    카테고리 별 맵 표시 기능 추가

--- TravelMagazine/Restaurant/RestaurantMapViewController.swift
+++ TravelMagazine/Restaurant/RestaurantMapViewController.swift
@@ -6,24 +6,76 @@
 //
 
 import UIKit
+import MapKit
 
 class RestaurantMapViewController: UIViewController {
+    
+    // 새싹좌표
+    let center = CLLocationCoordinate2D(latitude: 37.517857, longitude: 126.886714)
+    
+    let list = RestaurantList().restaurantArray
+    
+    var annotaions: [MKPointAnnotation] {
+        var arr: [MKPointAnnotation] = []
+        for res in list{
+            let point = CLLocationCoordinate2D(latitude: res.latitude, longitude: res.longitude)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = point
+            annotation.title = res.name
+            annotation.subtitle = res.category
+            arr.append(annotation)
+        }
+        return arr
+    }
+    var filterAnnotaions: [MKPointAnnotation] = []
 
+    @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+//        mapView.addAnnotations(annotaions)
+        
+        let barButton = UIBarButtonItem(title: "모두", style: .plain, target: self, action: #selector(barButtonClick))
+        navigationItem.rightBarButtonItem = barButton
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func barButtonClick() {
+//        mapView.removeAnnotations(annotaions)
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "모두", style: .default) { a in
+            self.makeAnnotations(a.title ?? "")
+        }
+        let action2 = UIAlertAction(title: "카페", style: .default) { a in
+            self.makeAnnotations(a.title ?? "")
+        }
+        let action3 = UIAlertAction(title: "한식", style: .default) { a in
+            self.makeAnnotations(a.title ?? "")
+        }
+        alert.addAction(action1)
+        alert.addAction(action2)
+        alert.addAction(action3)
+        present(alert, animated: true)
     }
-    */
+    
+    private func makeAnnotations(_ category: String) {
+        mapView.removeAnnotations(mapView.annotations)
+        guard category != "모두" else {
+            mapView.addAnnotations(annotaions)
+            return
+        }
+        var filterAnnotations: [MKPointAnnotation] = []
+        for res in list.filter({ $0.category == category }) {
+            let point = CLLocationCoordinate2D(latitude: res.latitude, longitude: res.longitude)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = point
+            annotation.title = res.name
+            annotation.subtitle = res.category
+            filterAnnotations.append(annotation)
+        }
+        mapView.addAnnotations(filterAnnotations)
+    }
+
+
 
 }
+
