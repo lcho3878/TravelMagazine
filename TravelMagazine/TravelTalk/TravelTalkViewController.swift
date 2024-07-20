a85648bad5a1931ec681336622a162e115a85de5
lcho3878 <leech3878@naver.com>
2024-07-20 18:09:45 +0900

    Feat: 테이블뷰 삭제 및 컬렉션뷰 적용

--- TravelMagazine/TravelTalk/TravelTalkViewController.swift
+++ TravelMagazine/TravelTalk/TravelTalkViewController.swift
@@ -6,66 +6,83 @@
 //
 
 import UIKit
+import SnapKit
 
 class TravelTalkViewController: UIViewController {
     
     let chatList = mockChatList
-    var filterList: [ChatRoom] = []{
-        didSet{
-            talkTableView.reloadData()
+    var filterList: [ChatRoom] = mockChatList{
+        didSet {
+            updateSnapshot()
         }
     }
+    
+    private var dataSource: UICollectionViewDiffableDataSource<String, ChatRoom>!
+    
+    private let layout = {
+        var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        configuration.showsSeparators = false
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        return layout
+    }()
+    
+    private lazy var talkCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
 
     @IBOutlet var searchBar: UISearchBar!
-    @IBOutlet var talkTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureTableView()
         configureSearchBar()
-    }
-
-    private func configureTableView() {
-        filterList = chatList
-        talkTableView.rowHeight = 80
-        talkTableView.delegate = self
-        talkTableView.dataSource = self
-        talkTableView.register(UINib(nibName: TravelTalkTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: TravelTalkTableViewCell.identifier)
+        configureDataSource()
+        updateSnapshot()
+        view.addSubview(talkCollectionView)
+        talkCollectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom)
+            $0.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
+    
+}
+
+extension TravelTalkViewController {
+    private func configureDataSource() {
+        var registration: UICollectionView.CellRegistration<UICollectionViewListCell, ChatRoom>!
+        registration = UICollectionView.CellRegistration{ cell, indexPath, chatroom in
+            var content = UIListContentConfiguration.valueCell()
+            content.text = chatroom.chatroomName
+            content.secondaryText = chatroom.chatList.last?.message
+            if chatroom.chatroomImage.count == 1 {
+                content.image = UIImage(named: chatroom.chatroomImage.first ?? "")
+            }
+            content.imageToTextPadding = 10
+            
+            cell.contentConfiguration = content
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: talkCollectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: itemIdentifier)
+            return cell
+        })
+    }
+    
+    private func updateSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<String, ChatRoom>()
+        snapshot.appendSections(["talk"])
+        snapshot.appendItems(filterList, toSection: "talk")
+        dataSource.apply(snapshot)
+    }
+    
+ 
+}
+
+
+extension TravelTalkViewController: UISearchBarDelegate {
     private func configureSearchBar() {
         searchBar.delegate = self
         searchBar.placeholder = "친구 이름을 검색해보세요"
     }
-}
-
-extension TravelTalkViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filterList.count
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: TravelTalkTableViewCell.identifier, for: indexPath) as? TravelTalkTableViewCell else {
-            return UITableViewCell()
-        }
-        
-        let chatRoom = filterList[indexPath.row]
-        cell.configureCell(chatRoom, searchBar.text!)
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let chatRoom = filterList[indexPath.row]
-        let sb = UIStoryboard(name: "Chatting", bundle: nil)
-        guard let vc = sb.instantiateViewController(withIdentifier: ChattingViewController.identifier) as? ChattingViewController else { return }
-        vc.chatRoom = chatRoom
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-}
-
-extension TravelTalkViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         guard searchText != "" else {
             filterList = chatList
